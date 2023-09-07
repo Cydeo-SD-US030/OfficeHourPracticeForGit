933b8373ce4c8133d0b0d9a30943b6f4cc17f8eb
oscar <oscar@cybertekschool.com>
2023-09-06 20:42:24 -0400

    created arraylist object in a polymorphic way

--- src/SetExamples.java
+++ src/SetExamples.java
@@ -1,7 +1,14 @@
+import com.sun.jdi.event.StepEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SetExamples {
     public static void main(String[] args) {
 
         String str = "uuuuuuoooooorrrrrrttttthhhhhllllkkkkk";
+
+        List<String> list = new ArrayList<>();
 
 
 
